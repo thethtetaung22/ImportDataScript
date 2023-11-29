7e46f765ccde536bad019512b0920ba48dddb544
Thet Htet Aung <thet@tractive.com.my>
2023-11-29 17:09:02 +0800

    Update db

--- withAsync.js
+++ withAsync.js
@@ -6,8 +6,8 @@ const dbConfig = {
     host: '127.0.0.1',
     port: '3306',
     user: 'root',
-    password: 'password',
-    database: 'testdb'
+    password: 'mmPSW@1412',
+    database: 'hcoolairconservices'
 };
 
 
